2a462f61f586b74b64829ea52313901d4c453c00
IsaPortuguez <isabel210399@gmail.com>
2021-05-27 22:04:15 -0600

    Cambiando idioma

---
+++ ClientTFTP/src/clienttftp/ClientTFTP.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienttftp;
+
+import Client.Client;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Isabel
+ */
+public class ClientTFTP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        String ip = JOptionPane.showInputDialog(null, "Ingrese la ip");
+        Client myClient = new Client(69, ip);
+        myClient.start();
+    }
+    
+}
